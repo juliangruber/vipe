d0513bd5357c6964c93859e26dc8bb9a79f1b9b4
Julian Gruber <julian@juliangruber.com>
2014-05-22 21:09:00 +0200

    add node implementation, just for fun

---
+++ vipe.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+/**
+ * This is a node implementation of the shell script,
+ * just for fun :) vipe.sh is what's being used.
+ */
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+var concat = require('concat-stream');
+var run = require('comandante');
+
+var tmp = '/tmp/vipe.' + Math.random() + '.txt';
+fs.touchSync(tmp);
+
+process.stdin.isTTY()
+  ? start('')
+  : process.stdin.pipe(concat(start));
+
+function start(stdin){
+  var ed = process.env.EDITOR || 'vi';
+  run(ed, [tmp], { stdio: 'pipe' })
+  .on('exit', function(){
+    process.stdout.write(fs.readFileSync(tmp));
+    fs.unlinkSync(tmp);
+  });
+}
+
